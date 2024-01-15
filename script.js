d2865854f06ddefd7514dd2bec62e2662013cac6
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-15 14:56:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* 
+
+JavaScript Exercise: Calculate Triangle Area
+Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.
+
+*/
+
 // var
 const output = document.querySelector("#output");
 const side1 = 5;
